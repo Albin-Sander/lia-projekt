c0576d43c43d926f060823429941cd850e8e1298
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2021-02-23 13:40:41 +0100

    server express update

--- cleanroom/src/App.js
+++ cleanroom/src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import Login from "./Components/login";
-const db = require("../database/cleanroom_database")
-
-db.getConnection(function(err, connection) {
-  // here u can run queries
-})
-
-
+import './App.sass';
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,9 +10,19 @@ import {
   useParams
 } from "react-router-dom";
 
-import './App.sass';
 
 
+/*db.getConnection(function(err, connection) {
+  if (err) {
+    throw console.log("Error when establishing a connection " + err)
+  }
+  // here u can run queries
+  let sql = "CREATE TABLE rooms (Room_id INT NOT NULL UNIQUE AUTO_INCREMENT, Room_nr INT NOT NULL UNIQUE, Room_size VARCHAR(1) NOT NULL, Room_booked INT NOT NULL, Priority_nr INT, Room_ready INT, Room_ready_time DATETIME, Room_start_time DATETIME, Cleaner_reponsible VARCHAR(45) UNIQUE, PRIMARY KEY (Room_id))";
+  connection.query(sql, function(err, result) {
+    if (err) throw console.log('Error while creating table ' + err)
+    console.log('Table "rooms" created')
+  })
+})*/
 
 function App() {
 
---
+++ cleanroom/src/database/cleanroom_server.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql');
+const express = require("express")
+const app = express()
+
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'admin',
+  database : 'dummy_db',
+  port: "3306"
+});
+
+connection.connect(function(err) {
+  if(err) throw err;
+  console.log("Connected to database!");
+});
+
+
+app.get('/hello', function (req, res) {
+   // here u can run queries
+   // INSERT, UPDATE
+    let sql = "CREATE TABLE rooms (Room_id INT NOT NULL UNIQUE AUTO_INCREMENT, Room_nr INT NOT NULL UNIQUE, Room_size VARCHAR(1) NOT NULL, Room_booked INT NOT NULL, Priority_nr INT, Room_ready INT, Room_ready_time DATETIME, Room_start_time DATETIME, Cleaner_reponsible VARCHAR(45) UNIQUE, PRIMARY KEY (Room_id))";
+    connection.query(sql, function(err, result) {
+      if (err) throw console.log('Error while creating table ' + err)
+      console.log('Table "rooms" created')
+    })
+  res.send('hello world')
+});
+
+
+app.get('/topics')
+  
+app.listen(5000)
