9bd71e647f6092a61572305115da5a80ad128f10
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2021-02-26 17:56:42 +0100

    Update nav.js

--- cleanroom/src/Components/nav.js
+++ cleanroom/src/Components/nav.js
@@ -27,16 +27,41 @@ export default function nav() {
           </a>
         </div>
       {/* Logo text ends 
- 
- 
+        <li>
+              <Link to="/">Home</Link>
+            </li>
+            {(!sessionStorage.getItem("username")) ? (
+              <li>
+              <Link to="/login">Login</Link>
+              </li>
+            ) : (
+              <li>
+              <Link to="/logout">Logout</Link>
+              </li>
+            )}
+
+            <li>
+              <Link to="/rooms">Rooms</Link>
+          </li>
 
       */}
   
         <nav>
           <ul class="list-reset md:flex md:items-center">
             <li class="md:ml-4">
-              <Link class="block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0" to="/login">Home</Link>     
+              <Link class="block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0" to="/">Home</Link>
             </li>
+
+            {(!sessionStorage.getItem("username")) ? (
+              <li class="md:ml-4">
+              <Link to="/login" class="block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0">Login</Link>
+              </li>
+            ) : (
+              <li class="md:ml-4">
+              <Link to="/logout" class="block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0">Logout</Link>
+              </li>
+            )}
+            
             <li class="md:ml-4">
               <Link class="border-t block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0" to="/rooms">Rooms</Link>
             </li>   
